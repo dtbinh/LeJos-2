dc6af283dfba72e5de9049c5af45650a4a5a778b
eekkelund <eetuveikko@luukku.com>
2016-01-28 10:27:17 +0200

    Clean the code

--- LeJos/src/navirobo/Main.java
+++ LeJos/src/navirobo/Main.java
@@ -1,7 +1,11 @@
 package navirobo;
 
+import lejos.robotics.navigation.DestinationUnreachableException;
+
 public class Main {
-	public static void main (String[] args){
-		Pilot pilot = new Pilot();
+	public static void main (String[] args) throws DestinationUnreachableException{
+		Pilott pilot = new Pilott();
+		Naviguy naviG = new Naviguy(pilot);
+		naviG.start();
 	}
 }
--- LeJos/src/navirobo/Naviguy.java
+++ LeJos/src/navirobo/Naviguy.java
@@ -1,35 +1,81 @@
 package navirobo;
 
-
-
 import lejos.robotics.mapping.*;
+import lejos.robotics.navigation.DestinationUnreachableException;
+import lejos.robotics.navigation.Navigator;
+import lejos.robotics.navigation.Pose;
+import lejos.robotics.navigation.Waypoint;
+import lejos.robotics.pathfinding.AstarSearchAlgorithm;
 import lejos.robotics.pathfinding.FourWayGridMesh;
+import lejos.robotics.pathfinding.NodePathFinder;
+import lejos.robotics.pathfinding.Path;
+import lejos.robotics.pathfinding.*;
 import lejos.robotics.geometry.*;
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.localization.PoseProvider;
 
 public class Naviguy {
-	static Rectangle area = new Rectangle(0, 0, 150, 100);
-	static Line[] lines = new Line[6];
+	static Rectangle area = null;
 
-	public Naviguy() {
+	static Line[] lines;
+	static LineMap map;
+	static FourWayGridMesh fwgm;
+	private int waypointNum = 1;
+	Waypoint w;
+	Pose pose;
+	PoseProvider posi;
+	Navigator n;
 
-
-		lines[0] = new Line(0, 0, 150, 0);
-
-		lines[1] = new Line(0, 0, 0, 100);
-
-		lines[2] = new Line(150, 0, 150, 100);
-
-		lines[3] = new Line(0, 100, 150, 100);
-
-		lines[4] = new Line(30,20,30,60);
-		lines[5] = new Line(60,70,100,70);
-
-
-
+	public Naviguy(Pilott p) {
+		posi = new OdometryPoseProvider(p.getPilot());
+		/// set position
+		pose = new Pose(0, 0, 90);
+		posi.setPose(pose);
+		n = new Navigator(p.getPilot(), posi);
 	}
-	public static void main (String[] args){
-		Naviguy n = new Naviguy();
-		LineMap map = new LineMap(lines, area);
-		FourWayGridMesh fwgm = new FourWayGridMesh(map, 10, 10);
+
+	public FourWayGridMesh makeGrid() {
+
+		lines = new Line[3];
+		area = new Rectangle(0, 0, 137, 159);
+		lines[0] = new Line(38, 65, 85, 65);
+
+		lines[1] = new Line(85, 65, 85, 107);
+
+		lines[2] = new Line(37, 120, 37, 159);
+		map = new LineMap(lines, area);
+		fwgm = new FourWayGridMesh(map, 10, 10);
+		return fwgm;
+	}
+
+	public Path makePath(FourWayGridMesh fwgm) throws DestinationUnreachableException {
+		AstarSearchAlgorithm algo = new AstarSearchAlgorithm();
+		PathFinder pf = new NodePathFinder(algo, fwgm);
+		switch (waypointNum) {
+		case 1:
+			w = new Waypoint(120, 2);
+			break;
+		case 2:
+			w = new Waypoint(70, 80);
+			break;
+		case 3:
+			w = new Waypoint(10, 150);
+			break;
+		}
+		Path p = pf.findRoute(posi.getPose(), w);
+		return p;
+	}
+
+	public int getWaypointNum() {
+		return waypointNum;
+	}
+
+	public void start() throws DestinationUnreachableException {
+
+		while (getWaypointNum() <= 3) {
+			n.followPath(makePath(makeGrid()));
+			n.waitForStop();
+		}
+
 	}
 }
